01355f9420f5fc4008887e1250f0909d39112e03
Matthew.Deshotel <mdeshotel@dewberry.com>
2025-09-09 16:19:11 -0500

    better handle vmin/vmax

--- swe_processing/swe_mapping/utility/swe_minmax.py
+++ swe_processing/swe_mapping/utility/swe_minmax.py
@@ -12,14 +12,14 @@ _global_max = float("-inf")
 logger = logging.getLogger(__name__)
 
 
-def get_minmax(current_data: np.ndarray) -> tuple:
+def get_minmax(current_data: np.ndarray, vmin, vmax) -> tuple:
     """Update the global min/max with current data.
 
     Returns:
         The current global values
 
     """
-    global _global_min, _global_max
+    # global _global_min, _global_max
 
     if np.isnan(current_data).all():
         logger.warning("current_data contains only NaNs, skipping min/max calculation.")
@@ -29,8 +29,8 @@ def get_minmax(current_data: np.ndarray) -> tuple:
         current_min = np.nanmin(current_data)
         current_max = np.nanmax(current_data)
 
-    _global_min = min(_global_min, current_min)
-    _global_max = max(_global_max, current_max)
+    _global_min = min(vmin, current_min)
+    _global_max = max(vmax, current_max)
 
     return _global_min, _global_max
 
